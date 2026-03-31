a2e6361c910a6186b422f73fc5fb59b9d0e24585
Denay2468 <denayaung13@gmail.com>
2026-03-31 18:08:26 +0630

    code in input function

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -5,6 +5,28 @@ using namespace std;
 int arr[20]; 
 int n;       
 void input(){
+    int i;
+    while (true)
+    {
+        cout << "Enter the number of elements in the array: "; 
+        cin >> n; 
+
+        if (n <= 20)
+            break; 
+        else
+        {
+            cout << "\nArray can have a maximum of 20 elements.\n"; 
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Enter Array Elements" << endl;
+    cout << "=====================" << endl;
+    for (int i = 0; i < n; i++) 
+    {
+        cout << "Data-" << (i + 1) << ": ";
+        cin >> arr[i]; 
+    }
 
 }
 void bubblesortArray(){
@@ -15,5 +37,5 @@ void display(){
 }
 
 int main(){
-    
+
 }

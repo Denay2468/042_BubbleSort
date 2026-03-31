e88a4784707bea63ba23fe93cf2efc7d173a451e
Denay2468 <denayaung13@gmail.com>
2026-03-31 18:13:39 +0630

    code in display

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -62,7 +62,23 @@ void bubblesortArray(){
 }
 void display(){
 
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Sorted Array Elements" << endl;
+    cout << "==========================" << endl;
+    cout << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j]; 
+        if (j < n - 1)
+        {
+            cout << "-->";
+        }
+    }
+    
+
 }
+
 
 int main(){
 
